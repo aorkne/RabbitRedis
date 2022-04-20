24321fb85de0a42d810b46c43875ef5cc781d52e
aorkne <aorkne@gmail.com>
2022-04-20 10:35:12 +0300

    start

---
+++ Domain/Ticket.cs
@@ -0,0 +1,17 @@
+﻿namespace RabbitRedis.Domain;
+
+public class Ticket
+{
+    public Guid Id { get; set; }
+    public string Message { get; set; }
+    public TicketType Type { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
+
+public enum TicketType
+{
+    Default = 1,
+    Plus = 2,
+    Premium = 3,
+    Vip = 4
+}
